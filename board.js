999f260e884e43b3e03c5fbab4b76d27227c8765
macbook_of_L <ds2lvg@gmail.com>
2020-01-12 11:51:36 +0900

    테트로미노 하드코딩으로 만들기

---
+++ board.js
@@ -0,0 +1,14 @@
+class Board {
+  grid;
+  // 보드 초기화
+  reset() {
+    this.grid = this.getEmptyBoard();
+  }
+  // 행(20)만큼 length를 가진 객체를 배열로 만들껀데 열(10)을 0으로 배열로 만들어라
+  getEmptyBoard() {
+    return Array.from(
+      {length: ROWS}, () => Array(COLS).fill(0)
+    );
+  }
+}
+
--- main.js
+++ main.js
@@ -7,3 +7,15 @@ ctx.canvas.height = ROWS * BLOCK_SIZE;
 
 // 블록 크기 변경: 매번 BLOCK_SIZE로 계산할 필요가 없이 블록의 크기를 1로 취급
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+
+let board = new Board();
+
+function play() {
+  board.reset();
+  console.table(board.grid);
+
+  let piece = new Piece(ctx);
+  piece.draw();
+
+  board.piece=piece;
+}
