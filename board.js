9361e057e6ee004f14c4247f9656b27c632dcdf6
macbook_of_L <ds2lvg@gmail.com>
2020-01-12 13:37:56 +0900

    테트로미노 충돌 감지

--- board.js
+++ board.js
@@ -11,8 +11,30 @@ class Board {
     );
   }
 
+  insideWalls(x) {
+    return x >= 0 && x < COLS;
+  }
+
+  aboveFloor(y) {
+    return y <= ROWS;
+  }
+
+  notOccupied(x, y) {
+    return this.grid[y] && this.grid[y][x] === 0;
+  }
+
   valid(p) {
-    return true;
+    // 조각의 모든 블록 좌표를 계산하고 유효한 위치인지 확인한다
+    return p.shape.every((row, dy) => {
+      return row.every((value, dx) => {
+        let x = p.x + dx;
+        let y = p.y + dy;
+        return (
+          value === 0 ||
+          (this.insideWalls(x) && this.aboveFloor(y) && this.notOccupied(x, y))
+        );
+      });
+    });
   }
 }
 
--- piece.js
+++ piece.js
@@ -17,13 +17,14 @@ class Piece {
       [2, 2, 2],
       [0, 0, 0]
     ];
-
+    // 시작 위치
     this.x = 3;
     this.y = 0;
   }
 
   draw() {
     this.ctx.fillStyle=this.color;
+    // shape의 셀을 순회하면서 0보다 크면 색을 칠한다.
     this.shape.forEach((row, y) => {
       row.forEach((value, x) => {
         if(value > 0) this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
